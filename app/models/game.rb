3bb4b83b91799d23a9c632f2bdc50137d2c374ac
Monty Lennie <montylennie@gmail.com>
2017-01-23 18:36:02 +0000

    fix validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,12 @@
 class Game < ActiveRecord::Base
   belongs_to :user
 
+  validates_presence_of :date_played
+  validates_presence_of :oponent_id
+  validates_presence_of :user_score
+  validates_presence_of :oponent_score
+  validates_presence_of :user_id
+
   def win?
     user_score > oponent_score
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
 
   has_many :games
-  validates_presence_of :date_played
-  validates_presence_of :oponent_id
-  validates_presence_of :user_score
-  validates_presence_of :oponent_score
-  validates_presence_of :user_id
-
+ 
   def score
     Game.where(user_id: id).sum("user_score")
   end
