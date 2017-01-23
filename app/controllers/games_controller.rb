67a8cfa4d14c77ba911e83f0c191f7d85091086c
Monty Lennie <montylennie@gmail.com>
2017-01-23 16:48:34 +0000

    add games index page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,17 @@ class GamesController < ApplicationController
 
   def create
     Game.create(game_params)
+    flash[:notice] = 'Successfully checked in'
+    redirect_to games_path
   end
 
   def index
+
+    if params[:user_id]
+      @games = Game.all
+    else
+      @games = Game.find_by(user_id: params[:user_id])
+    end
 
   end
 
